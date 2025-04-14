d13a15723f3315737f7ea50d29c8a33d38465632
radial-hks <radialjiajie@gmail.com>
2025-04-14 22:49:15 +0800

    build: add dotenv dependency for environment variable management

--- fastrtc_funasr_test.py
+++ fastrtc_funasr_test.py
@@ -1,17 +1,39 @@
 from fastrtc_funasr import get_stt_model
-from fastrtc import Stream, ReplyOnPause
+from fastrtc import Stream, ReplyOnPause,get_tts_model
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
 
+# 加载环境变量
+load_dotenv()
 
 stt_model = get_stt_model()
+tts_model = get_tts_model()
 
+client = OpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url=os.getenv("DEEPSEEK_BASE_URL")
+)
 
 
 def echo(audio):
     prompt = stt_model.stt(audio)
     print("用户输入:",prompt)
-    return prompt
+    # print("LLM输出:",prompt)
+    # response = client.chat.completions.create(
+    #     model="deepseek-chat",
+    #     messages=[
+    #         {"role": "user", "content": prompt}
+    #     ],
+    #     stream=False
+    #     )
+    # llm_prompt = response.choices[0].message.content
+    # print("LLM输出:",llm_prompt)
+    for audio_chunk in tts_model.stream_tts_sync(prompt):
+        yield audio_chunk
+    
 
 if __name__ == "__main__":
     # stream = Stream(ReplyOnPause(echo), modality="audio", mode="send-receive")
-    stream = Stream(ReplyOnPause(echo), modality="audio", mode="send")
+    stream = Stream(ReplyOnPause(echo), modality="audio", mode="send-receive")
     stream.ui.launch()
